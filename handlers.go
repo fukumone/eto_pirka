f84a039bb7a8d8aa47d00975a1f5e926133f4c82
t−fukui <fukuimone@gmail.com>
2016-04-11 17:08:47 +0900

    facebookログイン認証途中

--- handlers.go
+++ handlers.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"github.com/gin-gonic/gin"
+	// "github.com/stretchr/objx"
+	"github.com/stretchr/gomniauth"
 )
 
 func RootHandler() gin.HandlerFunc {
@@ -15,5 +19,66 @@ func AdminHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		router.LoadHTMLFiles("templates/layout.html", "templates/admin/index.html")
 		c.HTML(200, "layout.html", "admin/index.html")
+	}
+}
+
+func LoginHandler() gin.HandlerFunc {
+	provider, err := gomniauth.Provider("facebook")
+	if err != nil {
+		log.Fatalln("Error when trying to get provider", provider, "-", err)
+	}
+	loginUrl, err := provider.GetBeginAuthURL(nil, nil)
+	if err != nil {
+		log.Fatalln("Error when trying to GetBeginAuthURL for", provider, "-", err)
+	}
+
+	return func(c *gin.Context) {
+		c.Writer.Header()["Location"] = []string{loginUrl}
+		c.Writer.WriteHeader(http.StatusTemporaryRedirect)
+	}
+}
+
+func CallBackHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// provider, err := gomniauth.Provider("facebook")
+		// if err != nil {
+		// 	log.Fatalln("Error when trying to get provider", provider, "-", err)
+		// }
+
+		// // get the credentials
+		// creds, err := provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+		// if err != nil {
+		// 	log.Fatalln("Error when trying to complete auth for", provider, "-", err)
+		// }
+
+		// user, err := provider.GetUser(creds)
+		// if err != nil {
+		// 	log.Fatalln("Error when trying to get user from", provider, "-", err)
+		// }
+		// chatUser := &chatUser{User: user}
+
+		// m := md5.New()
+		// io.WriteString(m, strings.ToLower(user.Email()))
+		// chatUser.uniqueID = fmt.Sprintf("%x", m.Sum(nil))
+
+		// avatarURL, err := avatars.GetAvatarURL(chatUser)
+		// if err != nil {
+		// 	log.Fatalln("Error when trying to GetAvatarURL", "-", err)
+		// }
+
+		// // save some data
+		// authCookieValue := objx.New(map[string]interface{}{
+		// 	"userid":     chatUser.uniqueID,
+		// 	"name":       user.Name(),
+		// 	"avatar_url": avatarURL,
+		// }).MustBase64()
+
+		// http.SetCookie(w, &http.Cookie{
+		// 	Name:  "auth",
+		// 	Value: authCookieValue,
+		// 	Path:  "/"})
+
+		c.Writer.Header()["Location"] = []string{"/"}
+		c.Writer.WriteHeader(http.StatusTemporaryRedirect)
 	}
 }
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"os"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/signature"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
 )
 
 var router *gin.Engine
@@ -10,10 +14,16 @@ func main() {
 	router = gin.Default()
 	router.Static("/assets", "./assets")
 
-	authorized := router.Group("/admin", gin.BasicAuth(gin.Accounts{"root": "password",}))
+	// setup gomniauth
+	gomniauth.SetSecurityKey(signature.RandomKey(64))
+	gomniauth.WithProviders(
+		facebook.New(os.Getenv("FB_CLIENT_ID"), os.Getenv("FB_SECRET_KEY"), "http://localhost:3000/auth/callback/facebook"),
+	)
 
+	router.GET("/auth/login/facebook", LoginHandler())
+	router.GET("/auth/callback/facebook", CallBackHandler())
+	authorized := router.Group("/admin", gin.BasicAuth(gin.Accounts{os.Getenv("BasicAuthUSER"): os.Getenv("BasicAuthPASSWORD"),}))
 	authorized.GET("/", AdminHandler())
-
 	router.GET("/", RootHandler())
 	router.Run(":3000")
 }
